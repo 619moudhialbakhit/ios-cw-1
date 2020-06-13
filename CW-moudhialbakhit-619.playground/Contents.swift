209be152b34990c3b4a53fde425b435bad772f7c
moudhialbakhit619 <moudhialbakhit@gmail.com>
2020-06-13 19:16:08 +0300

    classwork

---
+++ CW-moudhialbakhit-619.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+let name = "moudi"
+var hobby1 = "photography"
+var hobby2 = "sports"
+var age = 16
+var height = 47
+print(name)
+print(hobby1)
+print(hobby2)
+print(age)
+print(height)
