b3f3008303eea9cdd3633318d45201044be40a0f
moudhialbakhit619 <moudhialbakhit@gmail.com>
2020-06-13 21:36:23 +0300

    calsswork

--- CW-moudhialbakhit-619.playground/Contents.swift
+++ CW-moudhialbakhit-619.playground/Contents.swift
@@ -1,12 +1,39 @@
 import UIKit
-
+// part1
 let name = "moudi"
 var hobby1 = "photography"
 var hobby2 = "sports"
 var age = 16
-var height = 47
-print(name)
-print(hobby1)
-print(hobby2)
-print(age)
-print(height)
+var height = 1.56
+print("my name is: ",name, " , and my age is: \(age) , and my \(height) is: \(height) , my hobbies are: \(hobby1) , \(hobby2)")
+
+// part2
+//Mariam
+let frindname = "Mariam"
+var frindnhobby1 = "draw"
+var frindhobby2 = "basketball"
+var frindage = 17
+var frindheight = 1.60
+
+// comparing
+name == frindname
+name != frindname
+
+hobby1 == frindnhobby1
+hobby1 != frindnhobby1
+hobby2 == frindhobby2
+hobby2 != frindhobby2
+
+age < frindage
+age > frindage
+age <= frindage
+age >= frindage
+age != frindage
+
+height == frindheight
+height != frindheight
+height < frindheight
+height > frindheight
+height <= frindheight
+height >= frindheight
+
